750f94be67af4ce446e28f4940bc4ecddebd4962
Col-R <2colewestin@gmail.com>
2021-10-27 06:50:26 -0500

    fixed type casting issue, added for loop

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,15 +1,17 @@
 public class FizzBuzz {
-    public String fizzBuzz (int number){
-        if (number % 3 == 0) {
-            if (number % 5 == 0){
-                System.out.println("FizzBuzz");
+    public static void fizzBuzz (int number){
+        for (int i=1; i<=number; i++){
+            if (number % 3 == 0) {
+                if (number % 5 == 0){
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            } else if(number % 5 == 0){
+                System.out.println("Buzz");
             } else {
-                System.out.println("Fizz");
+                System.out.println(number);
             }
-        } else if(number % 5 == 0){
-            System.out.println("Buzz");
-        } else {
-            System.out.println(number);
         }
     }
 }
